858e92ee780fbbd5476bd8e8a5999a42c2c344d3
Ali Ghaffari Sabet <aghs8055@gmail.com>
2024-08-03 20:48:46 +0330

    add Account and Individual models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from individuals.models import Individual
+
+
+class Account(models.Model):
+    owner = models.ForeignKey(Individual, on_delete=models.CASCADE, null=False)
+    balance = models.PositiveIntegerField(default=0, null=False)
--- individuals/models.py
+++ individuals/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
-# Create your models here.
+
+class Individual(models.Model):
+    first_name = models.CharField(max_length=63, null=False, blank=False)
+    last_name = models.CharField(max_length=63, null=False, blank=False)
+    national_id = models.CharField(max_length=10, validators=[MinLengthValidator(10)], null=False, blank=False)
