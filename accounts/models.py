34bca2d5e3f12bf38d9fbd4d4f2d9ac48b57fe43
Ali Ghaffari Sabet <aghs8055@gmail.com>
2024-08-04 08:50:51 +0330

    add str method and index on balance

--- accounts/models.py
+++ accounts/models.py
@@ -6,3 +6,11 @@ from individuals.models import Individual
 class Account(models.Model):
     owner = models.ForeignKey(Individual, on_delete=models.CASCADE, null=False)
     balance = models.PositiveIntegerField(default=0, null=False)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["balance"]),
+        ]
+
+    def __str__(self):
+        return f"{str(self.owner)}: {self.balance}"
--- individuals/models.py
+++ individuals/models.py
@@ -6,3 +6,6 @@ class Individual(models.Model):
     first_name = models.CharField(max_length=63, null=False, blank=False)
     last_name = models.CharField(max_length=63, null=False, blank=False)
     national_id = models.CharField(max_length=10, validators=[MinLengthValidator(10)], null=False, blank=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.national_id})"
